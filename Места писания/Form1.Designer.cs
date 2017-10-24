2bdacba2098f88f5953e763db964c66323c6a8dd
mikhan808 <kalyachin@mail.ru>
2017-10-24 17:27:39 +0300

    Добавлены имеющиеся файлы проекта

---
+++ Места писания/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Места_писания
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить место писания";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Бытие",
+            "Исход",
+            "Левит",
+            "Числа",
+            "Второзаконие",
+            "Иисус Навин",
+            "Книга Судей",
+            "Руфь",
+            "1 Царств",
+            "2 Царств",
+            "3 Царств",
+            "4 Царств",
+            "1 Паралипоменон",
+            "2 Паралипоменон",
+            "Ездра",
+            "Неемия",
+            "Есфирь",
+            "Иов",
+            "Псалтирь",
+            "Притчи",
+            "Екклесиаст",
+            "Песни Песней",
+            "Исаия",
+            "Иеремия",
+            "Плач Иеремии",
+            "Иезекииль",
+            "Даниил",
+            "Осия",
+            "Иоиль",
+            "Амос",
+            "Авдий",
+            "Иона",
+            "Михей",
+            "Наум",
+            "Аввакум",
+            "Софония",
+            "Аггей",
+            "Захария",
+            "Малахия",
+            "Матфей",
+            "Марк",
+            "Лука",
+            "Иоанн",
+            "Деяния",
+            "Иакова",
+            "1 Петра",
+            "2 Петра",
+            "1 Иоанна",
+            "2 Иоанна",
+            "3 Иоанна",
+            "Иуды",
+            "Римлянам",
+            "1 Коринфянам",
+            "2 Коринфянам",
+            "Галатам",
+            "Ефесянам",
+            "Филипийцам",
+            "Колосянам",
+            "1 Фессалоникийцам",
+            "2 Фессалоникийцам",
+            "1 Тимофею",
+            "2 Тимофею",
+            "Титу",
+            "Филимону",
+            "Евреям",
+            "Откровение"});
+            this.comboBox1.Location = new System.Drawing.Point(10, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(10, 30);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(10, 57);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(137, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Сегодня";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(83, 157);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 6;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "ОК";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Location = new System.Drawing.Point(110, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(339, 122);
+            this.panel1.TabIndex = 9;
+            this.panel1.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(307, 157);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 121);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(458, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 302);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Места писания/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+
+namespace Места_писания
+{
+    public partial class Form1 : Form
+    {
+        ArrayList dt;
+        ArrayList mp;
+        public Form1()
+        {
+            InitializeComponent();
+            dt = new ArrayList();
+            mp = new ArrayList();
+            if (File.Exists("daty"))
+            {
+                string[] temp = File.ReadAllLines("daty");
+                for (int i=0;i<temp.Length;i++)
+                {
+                    dt.Add(temp[i]);
+                }
+            }
+            if (File.Exists("mesta"))
+            {
+                string[] temp = File.ReadAllLines("mesta");
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    mp.Add(temp[i]);
+                }
+            }
+
+
+        }  
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            if (comboBox1.Text != "" && comboBox2.Text != "" && comboBox3.Text != "")
+            {
+                string str = comboBox1.Text + " " + comboBox2.Text + ":" + comboBox3.Text;
+                if(mp.Contains(str))
+                {
+                    MessageBox.Show("данное место уже есть в списке");
+                    panel1.Visible = false;
+                }
+                else
+                {
+                    mp.Add(str);
+                    string temp = Convert.ToString(dateTimePicker1.Value.Date);
+                    dt.Add(temp);
+                    string[] mest = new string[ mp.Count];
+                    for (int i=0;i<mest.Length;i++)
+                    {
+                        mest[i] = (string)mp[i];
+                    }
+                    string[] data = new string[dt.Count];
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        data[i] = (string)dt[i];
+                    }
+                    File.WriteAllLines("mesta",mest);
+                    File.WriteAllLines("daty", data);
+                    panel1.Visible = false;
+                    update();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 151; i++)
+                comboBox2.Items.Add(i);
+            for (int i = 1; i < 177; i++)
+                comboBox3.Items.Add(i);
+            update();
+
+
+        }
+        ArrayList Stack = new ArrayList();
+        ArrayList Stack1 = new ArrayList();
+        string text = "";
+        void poisk(string str)
+        {
+            int index = dt.IndexOf(str);
+            if(index>=0)
+            {
+                listBox1.Items.Add(mp[index]);
+                //text += (string)mp[index]+"\n";
+                Stack.Add(mp[index]);
+                Stack1.Add(dt[index]);
+                mp.RemoveAt(index);
+                dt.RemoveAt(index);
+                poisk(str);
+            }
+        }
+        void update()
+        {
+            string str = Convert.ToString(dateTimePicker2.Value.Date);
+            text = "";
+            listBox1.Items.Clear();
+            poisk(str);
+            //richTextBox1.Text = text;
+            for (int i = 0; i < Stack.Count; i++)
+            {
+                mp.Add(Stack[i]);
+                dt.Add(Stack1[i]);
+            }
+            Stack.Clear();
+            Stack1.Clear();
+        }
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            update();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            if (listBox1.SelectedIndex >=0)
+            {
+                int ind = listBox1.SelectedIndex;
+                int ind_of = mp.IndexOf(listBox1.Items[ind]);
+                mp.RemoveAt(ind_of);
+                dt.RemoveAt(ind_of);
+                string[] mest = new string[mp.Count];
+                for (int i = 0; i < mest.Length; i++)
+                {
+                    mest[i] = (string)mp[i];
+                }
+                string[] data = new string[dt.Count];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = (string)dt[i];
+                }
+                File.WriteAllLines("mesta", mest);
+                File.WriteAllLines("daty", data);
+                listBox1.Items.RemoveAt(ind);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button3.Visible = true;
+        }
+    }
+}
